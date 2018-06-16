0e9f2ac515dbdb24e43b8d8101a401614eb7e6d3
HungPham <pmhung07@gmail.com>
2018-06-16 12:24:44 +0700

    Base

---
+++ js/function.js
@@ -0,0 +1,22 @@
+﻿$(function () {
+
+    $('.user_act ').click(function(){
+        if( !$(this).hasClass('user_act_active') ) {
+            $('.user_popup_log').fadeIn(200);
+            $(this).addClass('user_act_active');
+        }else{
+            $('.user_popup_log').fadeOut(200);
+            $(this).removeClass('user_act_active');
+        }
+    });
+
+    $(document).mouseup(function(e) {
+        var container_target = $(".user_popup_log");
+        if (!container_target.is(e.target) && container_target.has(e.target).length === 0) 
+        {
+            $('.user_popup_log').fadeOut(200);
+            $('.user_act').removeClass('user_act_active');
+        }
+    });
+
+});
